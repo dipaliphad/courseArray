5e98af8f41567905e14cd554579525ebe7e2980a
rupesh palve <rupesh.palve48@outlook.com>
2022-09-21 16:48:09 -0400

    DepartmentArray

--- courseArray/Program.cs
+++ courseArray/Program.cs
@@ -10,17 +10,31 @@ namespace courseArray
     {
         static void Main(string[] args)
         {
-            int[] nums = { 1, 2, 3, 4 };
-            Course[] courses =
+            //int[] nums = { 1, 2, 3, 4 };
+            //Course[] courses =
+            //{
+            //    new Course{Id=1,Name="C++", fees=3500},
+            //    new Course{Id=2,Name="C#", fees=18000},
+            //    new Course{Id=3,Name="React", fees=15000},
+            //    new Course{Id=4,Name="Python", fees=13000},
+            //};
+            //foreach(Course c in courses)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            int[] nums= {1,2,3,4,5};
+            Department[] departments =
             {
-                new Course{Id=1,Name="C++", fees=3500},
-                new Course{Id=2,Name="C#", fees=18000},
-                new Course{Id=3,Name="React", fees=15000},
-                new Course{Id=4,Name="Python", fees=13000},
+                new Department{Id=1,Name="Mechanical", Location="pune"},
+                new Department{Id=2,Name="civil", Location="kothrud"},
+                new Department{Id=3,Name="E&Tc" ,Location="sangli"},
+                new Department{Id=4,Name="comp", Location="mumbai"},
+                new Department{Id=5,Name="IT" ,Location="nashik"},
             };
-            foreach(Course c in courses)
+            foreach (Department d in departments)
             {
-                Console.WriteLine(c);
+                Console.WriteLine(d);
             }
         }
     }
