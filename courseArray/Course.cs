0e42e69bdf40849c9037c6d3cae262da37ad893a
rupesh palve <rupesh.palve48@outlook.com>
2022-09-21 16:24:55 -0400

    Add project files.

---
+++ courseArray/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courseArray
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int fees { get; set; }
+
+        public override string ToString()
+        {
+            return $" Course detail: {Id} {Name} {fees}";
+        }
+    }
+}
---
+++ courseArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            Course[] courses =
+            {
+                new Course{Id=1,Name="C++", fees=3500},
+                new Course{Id=2,Name="C#", fees=18000},
+                new Course{Id=3,Name="React", fees=15000},
+                new Course{Id=4,Name="Python", fees=13000},
+            };
+            foreach(Course c in courses)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
